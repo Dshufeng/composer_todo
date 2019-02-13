672d053b5680eaf1c481492a1e0d7adc90f7641c
董书峰 <dongshufeng@dongshufengdeMacBook-Pro.local>
2019-02-13 17:50:31 +0800

    todo

---
+++ src/Dshufeng/composer_todo/Tools.php
@@ -0,0 +1,18 @@
+<?php
+namespace Dshufeng\composer_todo;
+
+/**
+ * 
+ */
+class Tools
+{
+	
+	public function __construct()
+	{
+		# code...
+	}
+ 
+	public static function Hello(){
+		echo "Hello";
+	}
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+require_once "./vendor/autoload.php";
+
+use Dshufeng\composer_todo\Tools;
+
+Tools::Hello();
